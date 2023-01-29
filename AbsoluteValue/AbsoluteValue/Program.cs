0af574bb085812fb1e9ece3832ee3d011c0af280
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:07:17 +0100

    Add files via upload

---
+++ AbsoluteValue/AbsoluteValue/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AbsoluteValue
+{
+    internal class AbsoluteValue
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a number to get its absolute value: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Math.Abs(a);
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Math.Abs(a);
+            Console.Write("The absolute value of " + a + " is ");
+            Console.Write(b);
+            Console.ReadLine();
+        }
+    }
+}
