fd8252dcc16793c13fd012fa20eecd3bb413579e
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:11:19 +0100

    Add files via upload

---
+++ IfYearIsLeap/IfYearIsLeap/IfYearIsLeap.cs
@@ -0,0 +1,18 @@
+﻿namespace IfYearIsLeap
+{
+    class IfYearIsLeapAssignment
+    {
+        static bool IfYearIsLeap(int year)
+        {
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Type a year to see if it is a leap year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write(year + " being a leap year is ");
+            Console.WriteLine(IfYearIsLeap(year));
+        }
+    }
+}
