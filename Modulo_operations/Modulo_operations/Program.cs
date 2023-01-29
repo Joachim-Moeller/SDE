a1c15b2781b5751c932c3e8452e2a39784f6a86d
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:04:32 +0100

    Add files via upload

---
+++ Modulo_operations/Modulo_operations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Modulo_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first integer");
+            double input_1 = (int)Convert.ToDouble(Console.ReadLine());
+
+            while (input_1 == 0)
+            {
+                Console.WriteLine("Number must not be zero");
+                Console.WriteLine("Enter first integer");
+                input_1 = (int)Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter second integer");
+            int input_2 = (int)Convert.ToDouble(Console.ReadLine());
+
+            while (input_2 == 0)
+            {
+                Console.WriteLine("Number must not be zero");
+                Console.WriteLine("Enter second integer");
+                input_2 = (int)Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter third integer");
+            int input_3 = (int)Convert.ToDouble(Console.ReadLine());
+
+            while (input_3 == 0)
+            {
+                Console.WriteLine("Number must not be zero");
+                Console.WriteLine("Enter third integer");
+                input_3 = (int)Convert.ToDouble(Console.ReadLine());
+            }
+            Console.Write("ModuloOperations = ");
+            Console.Write((input_1 % input_2) % input_3);
+            Console.ReadLine();
+        }
+    }
+}
