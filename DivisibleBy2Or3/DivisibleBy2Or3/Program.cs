98c8b28931ad72b20359ba8e2f8ad731332ef967
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:07:41 +0100

    Add files via upload

---
+++ DivisibleBy2Or3/DivisibleBy2Or3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Type 2 integers and get their multiplication if both are divisible by 2 or 3, otherwise their sum.");
+
+Console.Write("Type the 1st integer: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Type the 2nd integer: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("The result is: ");
+
+if (a % 2 == 0 && b % 2 == 0 || a % 3 == 0 && b % 3 == 0)
+{
+    Console.WriteLine(a * b);
+}
+else
+{
+
+    Console.Write(a + b);
+    Console.ReadLine();
+}
