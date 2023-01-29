d14b879f83d9d2fd8129495256fa3bfb00530164
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:09:28 +0100

    Add files via upload

---
+++ IfNumberIsEven/IfNumberIsEven/IfNumberIsEven.cs
@@ -0,0 +1,18 @@
+﻿namespace IfNumberIsEven
+{
+    class IfNumberIsEvenAssignment
+    {
+        static bool IfNumberIsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Type a number to see if it is an even number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num + " as it pertains to being an even number is ");
+            Console.WriteLine(IfNumberIsEven(num));
+        }
+    }
+}
