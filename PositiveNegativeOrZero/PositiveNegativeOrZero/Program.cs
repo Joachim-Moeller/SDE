1ea45ae33f649c59f4805dd2b9b9d11f1c0bdd8d
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:10:48 +0100

    Add files via upload

---
+++ PositiveNegativeOrZero/PositiveNegativeOrZero/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PositiveNegativeOrZero
+{
+    class PositiveNegativeOrZeroAssignment
+    {
+        static string PositiveNegativeOrZero(double num)
+        {
+            if (num > 0.0)
+            {
+                return "positive.";
+            }
+            else if (num < 0.0)
+            {
+                return "egative.";
+            }
+            return "zero.";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number to see if it is positive, negative, or zero: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.Write("The number entered is ");
+            Console.WriteLine(PositiveNegativeOrZero(num));
+            Console.ReadLine();
+        }
+    }
+}
