0ce0512641fc26c3cd9401a2eb9a85232d8d10f8
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:02:17 +0100

    Add files via upload

---
+++ Basic_Add/Basic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        public class AddTwoNumbers
+        {
+            public static void Main()
+            {
+                float num1, num2, num3;
+
+                Console.Write("Input first number: ");
+                num1 = float.Parse(Console.ReadLine());
+
+                Console.Write("Input second number to add: ");
+                num2 = float.Parse(Console.ReadLine());
+
+                Console.Write("Input third number to multiply the addition: ");
+                num3 = float.Parse(Console.ReadLine());
+
+                float result = (num1 + num2) * num3;
+                Console.WriteLine("Output: ({0} + {1}) x {2} = {3}", num1, num2, num3, result);
+            }
+        }
+    }
+}
