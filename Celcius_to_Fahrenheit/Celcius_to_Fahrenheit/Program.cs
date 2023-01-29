28238e29b16a3a951c753352d1579ea6fe39df14
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:03:01 +0100

    Add files via upload

---
+++ Celcius_to_Fahrenheit/Celcius_to_Fahrenheit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Celsius_to_Fahrenheit
+{
+    class Program
+    {
+        public class Celsius_to_Fahrenheit
+        {
+            public static void Main()
+            {
+                Console.Write("Enter Celsius degrees: ");
+                double celsius = Convert.ToDouble(Console.ReadLine());
+                if (celsius < -271.15)
+                    Console.WriteLine("Temperature below absolute zero!");
+                
+                if (celsius >= -271.15)
+                    Console.WriteLine("T = {0}F", celsius * 18 / 10 + 32);
+            }
+        }
+    }
+}
