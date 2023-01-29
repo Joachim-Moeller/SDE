49e6f012d53d047ff64d2eb5255ef0df17c8b6f0
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:05:09 +0100

    Add files via upload

---
+++ TheCubeOf/TheCubeOf/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Modulo_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number");
+            double number = (double)Convert.ToDouble(Console.ReadLine());
+
+            while (number == 0)
+            {
+                Console.WriteLine("Number must not be zero");
+                Console.WriteLine("Write a number");
+                number = (double)Convert.ToDouble(Console.ReadLine());
+            }
+            Console.Write("The cube of ");
+            Console.Write((double)number);
+            Console.Write(" is ");
+            Console.Write(number * number * number);
+
+            Console.ReadLine();
+        }
+    }
+}
