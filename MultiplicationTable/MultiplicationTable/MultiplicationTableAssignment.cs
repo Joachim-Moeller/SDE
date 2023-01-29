186b916055d4ff98b56611e9ccc7200e8d9f0582
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:14:38 +0100

    Add files via upload

---
+++ MultiplicationTable/MultiplicationTable/MultiplicationTableAssignment.cs
@@ -0,0 +1,46 @@
+﻿namespace MultiplicationTable
+{
+    class MultiplicationTableAssignment
+    {
+        static void MultiplicationTable()
+        {
+            for (int a = 1; a <= 10; a++)
+            {
+                for (int b = 1; b <= 10; b++)
+                {
+                    if (a == 1)
+                    {
+                        if (a * b < 10)
+                        {
+                            Console.Write($" {a * b} ");
+                        }
+                        else
+                        {
+                            Console.Write($"{a * b} ");
+                        }
+                    }
+                    else if (a > 1 && a < 10)
+                    {
+                        if (a * b < 10)
+                        {
+                            Console.Write($" {a * b} ");
+                        }
+                        else
+                        {
+                            Console.Write($"{a * b} ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write($"{a * b} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            MultiplicationTable();
+        }
+    }
+}
