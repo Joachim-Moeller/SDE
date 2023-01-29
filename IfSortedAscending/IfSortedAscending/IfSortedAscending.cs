b1117b65ee9dc54c1a4b2d6b0dbc2b70dc2401ec
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-01-29 19:10:03 +0100

    Add files via upload

---
+++ IfSortedAscending/IfSortedAscending/IfSortedAscending.cs
@@ -0,0 +1,28 @@
+﻿namespace IfSortedAscending
+{
+    class IfSortedAscendingAssignment
+    {
+        static bool IfSortedAscending(int[] arr)
+        {
+            return arr[0] <= arr[1] && arr[1] <= arr[2];
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = new int[3];
+            Console.WriteLine("Input 3 integers to see if they are sorted in ascending order.");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter the {0} integer", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine("Are those 3 integers written in ascending order?");
+            Console.Write(IfSortedAscending(a));
+        }
+    }
+}
