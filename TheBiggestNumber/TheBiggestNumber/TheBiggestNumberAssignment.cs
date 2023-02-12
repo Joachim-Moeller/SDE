512a57a0d644afecd994f94ce1873af93c60e4a3
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-12 17:45:55 +0100

    Add files via upload

---
+++ TheBiggestNumber/TheBiggestNumber/TheBiggestNumberAssignment.cs
@@ -0,0 +1,27 @@
+﻿namespace TheBiggestNumber
+{
+    class TheBiggestNumberAssignment
+    {
+
+        static int TheBiggestNumber(int[] arr)
+        {
+            int Biggest = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > Biggest)
+                {
+                    Biggest = arr[i];
+                }
+            }
+            return Biggest;
+            }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TheBiggestNumber(new int[] { 190, 291, 145, 209, 280, 200 }));
+            Console.WriteLine(TheBiggestNumber(new int[] { -9, -2, -7, -8, -4 }));
+            Console.ReadLine();
+        }
+    }
+}
