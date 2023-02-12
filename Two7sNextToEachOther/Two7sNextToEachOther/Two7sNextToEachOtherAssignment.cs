4b482a89d5834b483c5fc986e035744ec0ad507a
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-12 19:10:41 +0100

    Add files via upload

---
+++ Two7sNextToEachOther/Two7sNextToEachOther/Two7sNextToEachOtherAssignment.cs
@@ -0,0 +1,23 @@
+﻿namespace Two7sNextToEachOther
+{
+    internal class Two7sNextToEachOtherAssignment
+    {
+        static int Two7sNextToEachOther(int[] arr)
+        {
+            int Adjacent7s = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == 7 && arr[i + 1] == 7)
+                {
+                    Adjacent7s++;
+                }
+            }
+            return Adjacent7s;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Two7sNextToEachOther(new int[] { 8, 2, 5, 7, 9, 0, 7, 7, 3, 1 }));
+            Console.WriteLine(Two7sNextToEachOther(new int[] { 9, 4, 5, 3, 7, 7, 7, 3, 2, 5, 7, 7 }));
+        }
+    }
+}
