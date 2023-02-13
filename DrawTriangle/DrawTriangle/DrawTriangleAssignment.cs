c50d16fa84e3c3e9b6957cc6e40069a409be778a
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-13 02:25:43 +0100

    Add files via upload

---
+++ DrawTriangle/DrawTriangle/DrawTriangleAssignment.cs
@@ -0,0 +1,25 @@
+﻿namespace DrawTriangle
+{
+    internal class DrawTriangleAssignment
+    {
+        static void Main(string[] args)
+        {
+            int n, x, y, z;
+
+            n = 10; //antal rækker
+
+            for (x = 1; x <= n; x++) //main outer loop printer 10 rækker
+            {
+                for (y = 1; y <= n - x; y++) //printer mellemrummene
+                {
+                    Console.Write(" ");
+                }
+                for (z = 1; z <= x; z++) //printer x, dvs *
+                {
+                    Console.Write("* "); //mellemrum indsat efter * så det ligner en ordentlig trekant
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
