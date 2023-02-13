f649bc5c28fe114e3d91f844d4c1e4044c41b942
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-13 01:41:34 +0100

    Add files via upload

---
+++ FullSequenceOfLettersM/FullSequenceOfLettersM/FullSequenceOfLettersAssignment.cs
@@ -0,0 +1,24 @@
+﻿namespace FullSequenceOfLettersM
+{
+    internal class FullSequenceOfLettersAssignment
+    {
+        static string FullSequenceOfLetters(string word)
+        {
+            string FullSequence = string.Empty;
+            for (int i = word[0], j = 0; i <= word[1]; i++, j++)
+            {
+                FullSequence += (char)(word[0] + j);
+            }
+            return FullSequence;
+        }
+  
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two letters, 1st letter being before 2nd letter in the alphabet");
+            Console.WriteLine("and get full sequence of letters between them, themselves included: ");
+            string word = Console.ReadLine();
+            Console.Write("The full sequence is ");
+            Console.WriteLine(FullSequenceOfLetters(word));    
+        }
+    }
+}
