fcf3d5df886fa1ce13307d944dda1bd3994538fc
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-18 07:44:11 +0100

    Add files via upload

---
+++ IsPalindrome/IsPalindrome/IsPalindromeAssignment.cs
@@ -0,0 +1,23 @@
+﻿namespace IsPalindrome;
+
+internal class IsPalindromeAssignment
+{
+    private static bool IsPalindrome(string pal)
+    {
+        for (var i = 0; i < pal.Length / 2; i++)
+            if (pal[i] != pal[pal.Length - 1 - i])
+                return false;
+
+        return true;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput a word to see if it is a palindrome or not.\n");
+        Console.Write("Input string: ");
+        var pal = Console.ReadLine();
+        Console.WriteLine("----------------------------------------------------------------");
+        Console.Write("Your entered word being a palindrome is: ");
+        Console.WriteLine(IsPalindrome(pal));
+    }
+}
