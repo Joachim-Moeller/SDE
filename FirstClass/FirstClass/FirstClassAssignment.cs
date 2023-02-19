d4cab1c10d1ce15b404f5b2983d6cba2b486182f
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-19 18:02:09 +0100

    Add files via upload

---
+++ FirstClass/FirstClass/FirstClassAssignment.cs
@@ -0,0 +1,38 @@
+﻿namespace FirstClass;
+
+internal class FirstClassAssignment
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return "Hello! My name is " + Name;
+        }
+
+        private static void Main(string[] args)
+        {
+            var total = 3;
+            var persons = new Person[total];
+
+            Console.WriteLine("Enter names of three people.");
+
+            for (var i = 0; i < total; i++)
+            {
+                Console.Write("Enter name of {0} person: ", i);
+
+                persons[i] = new Person
+                {
+                    Name = Console.ReadLine()
+                };
+            }
+
+            Console.WriteLine("-------------------------------------------------------");
+
+            for (var i = 0; i < total; i++) Console.WriteLine(persons[i].ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
