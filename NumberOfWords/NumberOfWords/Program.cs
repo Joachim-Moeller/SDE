1b9d5b70cc35159bd53aeabbfb48cbbec63e7efb
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-19 15:20:41 +0100

    Add files via upload

---
+++ NumberOfWords/NumberOfWords/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NumberOfWords;
+
+internal class NumberOfWordsAssignment
+{
+    private static int NumberOfWords(string str)
+    {
+        var numberOfWords = 0;
+        for (var i = 1; i < str.Length; i++)
+            numberOfWords = char.IsWhiteSpace(str[i]) ? numberOfWords + 1 : numberOfWords;
+
+        return numberOfWords + 1;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput a string and get its number of words.\n");
+        Console.Write("Input string: ");
+        var str = Console.ReadLine();
+        Console.WriteLine("---------------------------------------------------");
+        Console.Write("Number of words in your entered string is ");
+        Console.WriteLine(NumberOfWords(str));
+    }
+}
