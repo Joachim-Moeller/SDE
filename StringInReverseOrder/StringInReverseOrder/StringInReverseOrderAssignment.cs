a4cfe3229da928f478d39fda65e6c7dfbd05bfbc
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-19 15:04:39 +0100

    Add files via upload

---
+++ StringInReverseOrder/StringInReverseOrder/StringInReverseOrderAssignment.cs
@@ -0,0 +1,22 @@
+﻿namespace StringInReverseOrder;
+
+internal class StringInReverseOrderAssignment
+{
+    private static string StringInReverseOrder(string str)
+    {
+        var reverseString = string.Empty;
+        for (var i = str.Length - 1; i >= 0; i--) reverseString += str[i];
+
+        return reverseString;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput a string to have it returned in reverse order.\n");
+        Console.Write("Input string: ");
+        var str = Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------");
+        Console.Write("Your reversed string: ");
+        Console.WriteLine(StringInReverseOrder(str));
+    }
+}
