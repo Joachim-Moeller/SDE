8e99bfd271e3382d0b2370a18a078afe6c69f65a
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-19 19:08:07 +0100

    Add files via upload

---
+++ PersonStudentTeacher/PersonStudentTeacher/PersonStudentTeacher.cs
@@ -0,0 +1,59 @@
+﻿namespace PersonStudentTeacher;
+
+public class Person
+{
+    protected int age;
+
+    public void Greet()
+    {
+        Console.WriteLine("Hello!");
+    }
+
+    public void SetAge(int n)
+    {
+        age = n;
+    }
+}
+
+public class Student : Person
+{
+    public void Study()
+    {
+        Console.WriteLine("I'm studying");
+    }
+
+    public void ShowAge()
+    {
+        Console.WriteLine("My age is {0} years old", age);
+    }
+}
+
+public class Teacher : Person
+{
+    public void Explain()
+    {
+        Console.WriteLine("I'm explaining");
+    }
+}
+
+public class PersonStudentTeacher
+{
+    public static void Main()
+    {
+        var myPerson = new Person();
+        myPerson.Greet();
+
+        var myStudent = new Student();
+        Console.Write("Input age of student: ");
+        myStudent.SetAge(Convert.ToInt32(Console.ReadLine()));
+        myStudent.Greet();
+        myStudent.ShowAge();
+        myStudent.Study();
+
+        var myTeacher = new Teacher();
+        Console.Write("Input age of teacher: ");
+        myTeacher.SetAge(Convert.ToInt32(Console.ReadLine()));
+        myTeacher.Greet();
+        myTeacher.Explain();
+    }
+}
