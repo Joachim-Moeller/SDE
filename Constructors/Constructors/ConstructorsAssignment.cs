aad2b3c1fcf9c6d7701849a9722508055e26ac1d
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-19 18:23:26 +0100

    Add files via upload

---
+++ Constructors/Constructors/ConstructorsAssignment.cs
@@ -0,0 +1,42 @@
+﻿namespace Constructors;
+
+internal class ConstructorsAssignment
+{
+    public static void Main(string[] args)
+    {
+        var total = 3;
+        var persons = new Person[total];
+
+        Console.WriteLine("Enter names of three people.");
+
+        for (var i = 0; i < total; i++)
+        {
+            Console.Write("Enter name of {0} person: ", i);
+            persons[i] = new Person(Console.ReadLine());
+        }
+
+        Console.WriteLine("-------------------------------------------------------");
+
+        for (var i = 0; i < total; i++) Console.WriteLine(persons[i].ToString());
+    }
+
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return "Hello! My name is " + Name;
+        }
+
+        Person()
+        {
+            Name = string.Empty;
+        }
+    }
+}
