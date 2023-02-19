11a276a5a9883e7edbf21c754b495620f0a73777
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-19 18:00:36 +0100

    Add files via upload

---
+++ LenghtOfString/LenghtOfString/LenghtOfStringAssignment.cs
@@ -0,0 +1,22 @@
+﻿namespace LenghtOfString;
+
+internal class LenghtOfStringAssignment
+{
+    private static int LengthOfAString(string str)
+    {
+        var length = 0;
+        foreach (var c in str) length++;
+
+        return length;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput a string to see its lenght\n");
+        Console.Write("Input string: ");
+        var str = Console.ReadLine();
+        Console.WriteLine("----------------------------------------------------------------");
+        Console.Write("The lenght of your string is ");
+        Console.Write(LengthOfAString(str));
+    }
+}
