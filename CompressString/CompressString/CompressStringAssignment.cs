e5ceb115749d9942cd5673be190d74e80ce7f083
Joachim Moeller <119821071+Joachim-Moeller@users.noreply.github.com>
2023-02-19 17:15:09 +0100

    Add files via upload

---
+++ CompressString/CompressString/CompressStringAssignment.cs
@@ -0,0 +1,37 @@
+﻿namespace CompressString;
+
+internal class CompressStringAssignment
+{
+    private static string CompressString(string str)
+    {
+        var count = 0;
+        var last = str[0];
+        var newStr = string.Empty;
+
+        foreach (var c in str)
+            if (c == last)
+            {
+                count++;
+            }
+            else
+            {
+                newStr += last.ToString() + count;
+                last = c;
+                count = 1;
+            }
+
+        newStr += last.ToString() + count;
+
+        return newStr;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput a string to get it in compressed format.\n");
+        Console.Write("Input string: ");
+        var str = Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------");
+        Console.Write("The compressed string: ");
+        Console.WriteLine(CompressString(str));
+    }
+}
